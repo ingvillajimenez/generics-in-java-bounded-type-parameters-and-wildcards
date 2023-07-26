64c212f397e99460508b6afba16f4f80e6ebe5f0
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-25 22:01:08 -0600

    Specifying Bounded Type Parameters

--- GenericsInJava/src/com/skillsoft/generics/Main.java
+++ GenericsInJava/src/com/skillsoft/generics/Main.java
@@ -6,19 +6,29 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-//        System.out.println("********** Invoke the isEven() method");
+        System.out.println("************* Invoke the isPrime() method");
+
+        System.out.println("Is 17 prime: " + MathUtil.isPrime(17));
+
+        System.out.println("Is 37 prime: " + MathUtil.isPrime(37));
+
+        System.out.println("Is 52 prime: " + MathUtil.isPrime(51));
+
+//        System.out.println("************* Invoke the isPrime() method");
 //
-//        System.out.println("Is fifty even: " + MathUtil.isEven("fifty")); // java.lang.ClassCastException. class java.lang.String cannot be cast to class java.lang.Number
-
-        System.out.println("*********** Invoke the isPrime() method");
-
-        System.out.println("Is seventeen prime: " + MathUtil.isPrime("seventeen")); // java.lang.ClassCastException. class java.lang.String cannot be cast to class java.lang.Number
-
-        System.out.println("Is today's date prime: " + MathUtil.isPrime(new Date()));
-
-//        System.out.println("********** Invoke the isEven() method");
+//        System.out.println("Is 17 prime: " + MathUtil.isPrime(17));
 //
-//        System.out.println("Is 50 even: "  + MathUtil.isEven(50));
+//        System.out.println("Is 17.2 prime: " + MathUtil.isPrime(17.2));
+//        java: method isPrime in class com.skillsoft.generics.MathUtil cannot be applied to given types;
+//        required: T
+//        found:    double
+//        reason: inference variable T has incompatible bounds
+//        lower bounds: java.lang.Integer
+//        lower bounds: java.lang.Double
+
+//        System.out.println("************* Invoke the isEven() method");
+//
+//        System.out.println("Is 50 even: " + MathUtil.isEven(50));
 //
 //        System.out.println("Is 103 even: " + MathUtil.isEven(103));
 //
@@ -26,12 +36,31 @@ public class Main {
 //
 //        System.out.println("Is 5000000 even: " + MathUtil.isEven(BigInteger.valueOf(5000000)));
 //
-//        System.out.println("*********** Invoke the isPrime() method");
+//        System.out.println("************* Invoke the isPrime() method");
 //
 //        System.out.println("Is 50 prime: " + MathUtil.isPrime(50));
 //
 //        System.out.println("Is 17 prime: " + MathUtil.isPrime(17));
 //
 //        System.out.println("Is 5000000 prime: " + MathUtil.isPrime(BigInteger.valueOf(5000000)));
+
+
+//        System.out.println("********** Invoke the isEven() method");
+//
+//        System.out.println("Is fifty even: " + MathUtil.isEven("fifty")); // compile-time error
+////        java: method isEven in class com.skillsoft.generics.MathUtil cannot be applied to given types;
+////        required: T
+////        found:    java.lang.String
+////        reason: inference variable T has incompatible bounds
+////        lower bounds: java.lang.Number
+////        lower bounds: java.lang.String
+//
+//        System.out.println("*********** Invoke the isPrime() method");
+//
+//        System.out.println("Is seventeen prime: " + MathUtil.isPrime("seventeen")); // compile-time error
+//
+//        System.out.println("Is today's date prime: " + MathUtil.isPrime(new Date())); // compile-time error
+
+
     }
 }
--- GenericsInJava/src/com/skillsoft/generics/MathUtil.java
+++ GenericsInJava/src/com/skillsoft/generics/MathUtil.java
@@ -2,27 +2,45 @@ package com.skillsoft.generics;
 
 public class MathUtil {
 
-    public static <T extends Object> boolean isEven(T element) {
-    // <T extends Object> is an implicit assumption for any type parameter
-        Number number = (Number) element;
-
-        return number.doubleValue() % 2 == 0;
+    public static <T extends Number> boolean isEven(T element) {
+        return element.doubleValue() % 2 == 0;
     }
 
-    public static <T extends Object> boolean isPrime(T element) {
-    // The type T specified standalone is referred to as an unbounded type
-        Number number = (Number) element;
-
-        int num = number.intValue() / 2;
+    public static <T extends Integer> boolean isPrime(T element) {
+    // Let's make this type specification more strict
+        int num = element.intValue() / 2;
 
         for (int i = 2; i <= num; i++) {
 
-            if (number.intValue() % i == 0) {
+            if (element.intValue() % i == 0) {
                 return false;
             }
         }
 
         return true;
     }
+
+//    public static <T extends Number> boolean isEven(T element) {
+//    // <T extends Number> is an example of bounded type parameter
+//        Number number = (Number) element;
+//
+//        return number.doubleValue() % 2 == 0;
+//    }
+//
+//    public static <T extends Number> boolean isPrime(T element) {
+//    // This T is also a bounded type parameter
+//        Number number = (Number) element;
+//
+//        int num = number.intValue() / 2;
+//
+//        for (int i = 2; i <= num; i++) {
+//
+//            if (number.intValue() % i == 0) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
 
 }
